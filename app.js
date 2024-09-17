1002b6b62aaf19928b78689b16718ac943682f05
jaskirat <jaskiratsingh251103@gmail.com>
2024-09-18 00:14:33 +0530

    routers

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require("express")
 const app = express();
-
 const cookie=require("cookie-parser");
 const path=require("path");
+const db=require("./config/mongoose")
 
+const userRouter=require("./routes/userRouter")
+const productRouter=require("./routes/productRouter")
+const customerRouter=require("./routes/customerRouter")
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookie())
 app.use(express.static(path.join(__dirname,"public")));
-app.set("view engine","ejs");
+
+app.use("/userRoute",userRouter);
+app.use("/productRoute",productRouter);
+app.use("/customerRoute",customerRouter);
+
 
 app.get("/",(req,res)=>{
     res.send("hlo");
