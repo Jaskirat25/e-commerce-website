b6e61bd6bf7612160cf034b6e0371d9becb12f4a
jaskirat <jaskiratsingh251103@gmail.com>
2024-09-21 23:25:25 +0530

    userrouter

--- models/user-model.js
+++ models/user-model.js
@@ -3,7 +3,19 @@ const mongoose = require("mongoose");
 
 
 const userSchema=mongoose.Schema({
+username:{
+    type:String,
+    required:true,
+    minlength:3
+},
+email:String,
+password:String,
+products:{
+    type:[String],
+    default:[]
 
+},
+gst:String
 
 })
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,24 @@
 const express=require("express");
 const router=express.Router()
-
+const User=require("../models/user-model")
 router.get("/",(req,res)=>{
     res.send("hdkfsjdfsi");
 });
+if(process.env.NODE_ENV==="development"){
+    router.get("/create", async (req,res)=>{
+        const users= await User.find();
+        if(users.length>0){
+          return  res.status(201).send("you cant create a new user")
+        }
+      let  {username,password,email}=req.body;
+const created= await User.create({
+username,
+email,
+password
+})
+res.status(200).send(created);
+    })
+};
+
 
 module.exports=router;
